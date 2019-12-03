29dcdc56d7aca63356f90881e1e984946e601bfe
CYBAI <cyb.ai.815@gmail.com>
2019-12-03 10:53:38 +0900

    Add day 1 part 2 solution

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -8,6 +8,10 @@ fn main() {
             .collect();
 
         println!("The sum of the fuel requirements is: {}", part1(&numbers));
+        println!(
+            "The sum of the fuel requirements for all of the modules on my spacecraft is: {}",
+            part2(&numbers)
+        );
     }
 }
 
@@ -19,10 +23,34 @@ fn part1(numbers: &[i32]) -> i32 {
     return numbers.iter().map(calc_fuel_requirement).sum();
 }
 
+fn part2(numbers: &[i32]) -> i32 {
+    return numbers
+        .iter()
+        .map(|n| {
+            let mut number = calc_fuel_requirement(n);
+            let mut result = 0;
+
+            while number > 0 {
+                result += number;
+                number = calc_fuel_requirement(&number);
+            }
+
+            return result;
+        })
+        .sum();
+}
+
 #[test]
 fn test_part1() {
     assert_eq!(part1(&vec![12]), 2);
     assert_eq!(part1(&vec![14]), 2);
     assert_eq!(part1(&vec![1969]), 654);
     assert_eq!(part1(&vec![100756]), 33583);
+}
+
+#[test]
+fn test_part2() {
+    assert_eq!(part2(&vec![14]), 2);
+    assert_eq!(part2(&vec![1969]), 966);
+    assert_eq!(part2(&vec![100756]), 50346);
 }
